f336de988b3e54b02f2b54f4006aa7f43059c561
wakatakatakataka <wakatakatakataka@gmail.com>
2021-04-02 16:33:33 +0900

    修正２

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,10 @@ class CommentsController < ApplicationController
     @prototype = Prototype.find(params[:prototype_id])
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to @prototype
+      redirect_to prototype_path(@comment.prototype)
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show" 
     end
   end
